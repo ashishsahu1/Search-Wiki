93728058e67dc32cdeb1f4d0d88d8d0c55ee81ed
ashishsahu1 <ashishsahu1999@hotmail.com>
2020-10-12 02:41:28 +0530

    Searching images added

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request
 import wikipedia
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
 
 app = Flask(__name__)
 
@@ -12,8 +16,17 @@ def wiki():
     if request.method == 'POST':
         term = request.form['term']
         data = wikipedia.summary(str(term))
-        return render_template("result.html", data = data)
+        imgurl = img(term.replace(" ",""))
+        return render_template("result.html", data = data, imgurl = imgurl)
     else:
         return render_template('login.html')
+
+
+def img(term):
+    html = urlopen('https://en.wikipedia.org/wiki/{}'.format(term))
+    bs = BeautifulSoup(html, 'html.parser')
+    images = bs.find_all('img', {'src':re.compile('.jpg')})
+    return images[0]['src']
+
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-import wikipedia
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
 
-print(wikipedia.summary("Tiger"))
+term = "evans"
+html = urlopen('https://en.wikipedia.org/wiki/{}'.format(term.replace(" ","")))
+bs = BeautifulSoup(html, 'html.parser')
+images = bs.find_all('img', {'src':re.compile('.jpg')})
+print(images[0]['src']+'\n')
+
