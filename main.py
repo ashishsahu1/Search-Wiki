25757b0c99cb5b5ef4bce84971e1a5d3755196d5
ashishsahu1 <ashishsahu1999@hotmail.com>
2020-10-12 16:12:57 +0530

    Successful implementation of images search
    added many phtoots feature in place of one.

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ def wiki():
     if request.method == 'POST':
         term = request.form['term']
         data = wikipedia.summary(str(term))
-        imgurl = img(term.replace(" ",""))
-        return render_template("result.html",term = term, data = data, imgurl = imgurl)
+        imgurllist = img(term.replace(" ",""))
+        return render_template("result.html",term = term, data = data, imgurllist = imgurllist, l=len(imgurllist))
     else:
         return render_template('result.html')
 
@@ -26,7 +26,10 @@ def img(term):
     html = urlopen('https://en.wikipedia.org/wiki/{}'.format(term))
     bs = BeautifulSoup(html, 'html.parser')
     images = bs.find_all('img', {'src':re.compile('.jpg')})
-    return images[0]['src']
+    lst=[]
+    for i in images:
+        lst.append(str(i['src']))
+    return lst
 
 if __name__ == "__main__":
     app.run(debug=True)
